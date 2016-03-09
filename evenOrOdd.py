38aa5d3846767c7b492df34eb1d9f65d818485b0
Inspector-Gidget <ajtockey@yahoo.com>
2016-03-08 21:05:07 -0600

    Even or Odd
    A simple even or odd program for my subservient Gidget-5000.

---
+++ evenOrOdd.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+getFunky = "Them numbers, baby."
+
+print("Gidget-5000: Hello, master. How may I service you?")
+responseA = input(">>> ")
+
+if responseA == getFunky:
+	print("Gidget-5000: Oooh, evens or odds. What will be the number? ")
+	usernum = input(">>> ")
+	try:
+		if (int(usernum) == 666): 
+			print("Gidget-5000: Fuck yes, the number of the beast. That number is even, hail Satan.")
+		elif (int(usernum) % 2 == 0):
+			print("Gidget-5000: Your number is even. Goodbye master, I'll be waiting for you.")
+		else:
+			print("Gidget-5000: Your number is odd. Goodbye master, I'll be waiting for you.")
+	except ValueError as err:
+		print("If you want to pleasure me, you must provide an integer.")
+
+else:
+	print("What? I don't recognize that response... Go away stranger!")
+
+
